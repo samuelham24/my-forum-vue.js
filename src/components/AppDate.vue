7db719f02ae7a29772135b69174c2de60580f324
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-05-30 17:58:35 +0700

    Seperate date sistem to AppDate.vue

---
+++ src/components/AppDate.vue
@@ -0,0 +1,31 @@
+<template>
+  <span :title="timestamp | humanFriendlyDate">{{timestamp | diffForHumans}}</span>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    timestamp: {
+      required: true,
+      type: Number // Commonly integer, but in .js just Number
+    }
+  },
+
+  filters: {
+    humanFriendlyDate (date) {
+      // access data and formatting in hover
+      return moment
+        .unix(date)
+        .locale('en') // language reference
+        .format('MMMM Do YYYY, h:mm:ss a')
+    },
+
+    diffForHumans (date) {
+      // count and show the last time post
+      return moment.unix(date).fromNow()
+    }
+  }
+}
+</script>
