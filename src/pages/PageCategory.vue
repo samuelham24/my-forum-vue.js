7aa7732d32d3e91f755a6642b2181542a80cc437
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-01 01:29:04 +0700

    Update PageCategory.vue

--- src/pages/PageCategory.vue
+++ src/pages/PageCategory.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="col-full">
     <h1>{{ category.name }}</h1>
-    <CategoryListItem :category="category"/>
+    <CategoryListItem :category="category" />
   </div>
 </template>
 
 <script>
-    import CategoryListItem from '@/components/CategoryListItem'
-    import sourceData from '@/data'
-    export default {
-      components: {
-        CategoryListItem
-      },
-      props: {
-        id: {
-          required: true,
-          type: String
-        }
-      },
-      computed: {
-        category () {
-          return sourceData.categories[this.id]
-        }
-      }
+import CategoryListItem from '@/components/CategoryListItem'
+export default {
+  components: {
+    CategoryListItem
+  },
+  props: {
+    id: {
+      required: true,
+      type: String
     }
+  },
+  computed: {
+    category () {
+      return this.$store.state.categories[this.id]
+    }
+  }
+}
 </script>
 
 <style scoped>
