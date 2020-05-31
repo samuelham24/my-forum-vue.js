df6490c3a77f71ebc9e8d1f5586aa814d0ae9643
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-01 04:25:39 +0700

    Update PageThreadShow.vue

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -31,14 +31,11 @@ export default {
       type: String
     }
   },
-  data () {
-    return {
-      thread: this.$store.state.threads[this.id],
-      newPostText: ''
-    }
-  },
-
   computed: {
+    thread () {
+      return this.$store.state.threads[this.id]
+    },
+
     posts () {
       const postIds = Object.values(this.thread.posts)
       return Object.values(this.$store.state.posts).filter(post =>
