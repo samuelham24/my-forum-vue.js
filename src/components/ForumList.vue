8b1f046ca05c0f61cf796de53483011ab6aecfe3
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-01 01:23:14 +0700

    Update ForumList.vue

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -1,16 +1,12 @@
 <template>
     <!-- ForumList to render multiple ForumListItems -->
-  <div class="forum-list">
-    <h2 class="list-title">
-      <a href="#">Forums</a>
-    </h2>
-
+    <div>
     <ForumListItem
       v-for="forum in forums"
       :forum="forum"
       :key="forum['.key']"
     />
-  </div>
+    </div>
 </template>
 
 <script>
