a4ddf5d46574a455eedf6334698614806217da99
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-01 01:30:01 +0700

    Update PageThreadShow.vue

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,19 +2,20 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <p>
-      By <a href="#" class="link-unstyled">Robin</a>, <AppDate :timestamp="thread.publishedAt"/>.
-      <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies by 3 contributors</span>
-    </p>	    
-    <PostList :posts="posts"/>	        
-      <PostEditor
-        @save="addPost"
-        :threadId="id"
-        />
+      By
+      <a href="#" class="link-unstyled">Robin</a>,
+      <AppDate :timestamp="thread.publishedAt" />.
+      <span
+        style="float:right; margin-top: 2px;"
+        class="hide-mobile text-faded text-small"
+      >3 replies by 3 contributors</span>
+    </p>
+    <PostList :posts="posts" />
+    <PostEditor @save="addPost" :threadId="id" />
   </div>
 </template>
 
 <script>
-import sourceData from '@/data'
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
 
@@ -32,7 +33,7 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id],
+      thread: this.$store.state.threads[this.id],
       newPostText: ''
     }
   },
@@ -40,18 +41,18 @@ export default {
   computed: {
     posts () {
       const postIds = Object.values(this.thread.posts)
-      return Object.values(sourceData.posts).filter(post =>
+      return Object.values(this.$store.state.posts).filter(post =>
         postIds.includes(post['.key'])
       )
     }
   },
 
   methods: {
-    addPost ({post}) {
+    addPost ({ post }) {
       const postId = post['.key']
-      this.$set(sourceData.posts, postId, post)
+      this.$set(this.$store.state.posts, postId, post)
       this.$set(this.thread.posts, postId, postId)
-      this.$set(sourceData.users[post.userId].posts, postId, postId)
+      this.$set(this.$store.state.users[post.userId].posts, postId, postId)
     }
   }
 }
