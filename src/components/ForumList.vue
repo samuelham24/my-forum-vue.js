1cc3597409ab0ea69572d94cc0f90ef2718499f1
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-01 04:24:26 +0700

    Update ForumList.vue

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -1,46 +1,24 @@
 <template>
-  <!-- Post editor to edit form and submit -->
-  <form @submit.prevent="save">
-    <div class="form-group">
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          class="form-input"
-          v-model="text"
-        ></textarea>
+    <!-- ForumList to render multiple ForumListItems -->
+    <div>
+    <ForumListItem
+      v-for="forum in forums"
+      :forum="forum"
+      :key="forum['.key']"
+    />
     </div>
-    <div class="form-actions">
-      <button class="btn-blue">Submit post</button>
-    </div>
-  </form>
 </template>
 
 <script>
+    import ForumListItem from './ForumListItem'
     export default {
+      components: {
+        ForumListItem
+      },
       props: {
-        threadId: {
-          required: true
-        }
-      },
-      data () {
-        return {
-          text: ''
-        }
-      },
-      methods: {
-        save () {
-          const postId = 'greatPost' + Math.random()
-          const post = {
-            text: this.text,
-            publishedAt: Math.floor(Date.now() / 1000),
-            threadId: this.threadId,
-            userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-            '.key': postId
-          }
-          this.text = ''
-          this.$emit('save', {post})
+        forums: {
+          required: true,
+          type: Array
         }
       }
     }
