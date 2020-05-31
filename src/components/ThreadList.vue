c6a823ab1e3bb210002528d444ba6b74bf0d738f
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-01 04:23:04 +0700

    Update ThreadList.vue

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -1,24 +1,77 @@
 <template>
-<!-- ThreadList to render multiple ThreadListItems -->
-  <div class="thread-list">
-    <h2 class="list-title">Threads</h2>
-    <ThreadListItem v-for="thread in threads" :thread="thread" :key="thread['.key']" />
-  </div>
+  <header class="header" id="header">
+
+    <router-link
+      :to="{name: 'Home'}"
+      class="logo"
+    >
+      <img src="../assets/img/vueschool-logo.svg">
+    </router-link>
+
+    <div class="btn-hamburger">
+      <!-- use .btn-humburger-active to open the menu -->
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+
+    <!-- use .navbar-open to open nav -->
+    <nav class="navbar">
+      <ul>
+        <!--<li class="navbar-item">-->
+          <!--<a href="index.html">Home</a>-->
+        <!--</li>-->
+        <!--<li class="navbar-item">-->
+          <!--<a href="category.html">Category</a>-->
+        <!--</li>-->
+        <!--<li class="navbar-item">-->
+          <!--<a href="forum.html">Forum</a>-->
+        <!--</li>-->
+        <!--<li class="navbar-item">-->
+          <!--<a href="thread.html">Thread</a>-->
+        <!--</li>-->
+        <!--&lt;!&ndash; Show these option only on mobile&ndash;&gt;-->
+        <!--<li class="navbar-item mobile-only">-->
+          <!--<a href="profile.html">My Profile</a>-->
+        <!--</li>-->
+        <!--<li class="navbar-item mobile-only">-->
+          <!--<a href="#">Logout</a>-->
+        <!--</li>-->
+
+        <li class="navbar-user">
+          <router-link :to="{name: 'Profile'}">
+            <img class="avatar-small" :src="user.avatar" alt="">
+            <span>
+                {{user.name}}
+                <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
+            </span>
+          </router-link>
+
+          <!-- dropdown menu -->
+          <!-- add class "active-drop" to show the dropdown -->
+          <div id="user-dropdown">
+            <div class="triangle-drop"></div>
+            <ul class="dropdown-menu">
+              <li class="dropdown-menu-item"><a href="profile.html">View profile</a></li>
+              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
-import ThreadListItem from './ThreadListItem' // import thread list item
-
-export default {
-  components: {
-    ThreadListItem
-  },
-
-  props: {
-    threads: {
-      required: true,
-      type: Array // pass each thread of the array to ThreadListItem component
+  import {mapGetters} from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters({
+        'user': 'authUser'
+      })
     }
   }
-}
 </script>
+
+<style scoped>
+</style>
