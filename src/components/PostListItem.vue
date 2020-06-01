c34732a7c89151e705ccad0ed43f7370ff33c5f3
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-02 01:43:35 +0700

    Update PostListItem.vue

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -1,41 +1,67 @@
 <template>
-  <!-- Post list item to show a post from user -->
   <div class="post">
     <div class="user-info">
       <a href="#" class="user-name">{{user.name}}</a>
+
       <a href="#">
-        <img class="avatar-large" :src="user.avatar" alt />
+        <img class="avatar-large" :src="user.avatar" alt="">
       </a>
+
       <p class="desktop-only text-small">{{userPostsCount}} posts</p>
     </div>
+
     <div class="post-content">
-      <div>{{post.text}}</div>
+      <template v-if="!editing">
+        <div>
+          {{post.text}}
+        </div>
+        <a @click.prevent="editing = true" href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
+      </template>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+          @cancel="editing = false"
+        />
+      </div>
+
     </div>
+
     <div class="post-date text-faded">
-      <AppDate :timestamp="post.publishedAt" />
+      <div v-if="post.edited" class="edition-info">edited</div>
+      <AppDate :timestamp="post.publishedAt"/>
     </div>
   </div>
 </template>
 
 <script>
-import {countObjectProperties} from '@/utils'
-
-export default {
-  props: {
-    post: {
-      required: true,
-      type: Object
+    import {countObjectProperties} from '@/utils'
+    import PostEditor from './PostEditor'
+    export default {
+      props: {
+        post: {
+          required: true,
+          type: Object
+        }
+      },
+      components: {
+        PostEditor
+      },
+      data () {
+        return {
+          editing: false
+        }
+      },
+      computed: {
+        user () {
+          return this.$store.state.users[this.post.userId]
+        },
+        userPostsCount () {
+          return countObjectProperties(this.user.posts)
+        }
+      }
     }
-  },
-  computed: {
-    // computed properties make sure the user will be updated if change happen in  this.$store.state
-    user () {
-      return this.$store.state.users[this.post.userId]
-    },
-
-    userPostsCount () {
-      return countObjectProperties(this.user.posts)
-    }
-  }
-}
 </script>
+
+<style scoped>
+</style>
