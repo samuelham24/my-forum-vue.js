8162384761d71170103ab0b4afd11645edf1fc2a
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-02 02:27:07 +0700

    Update PostListItem.vue

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -7,7 +7,7 @@
         <img class="avatar-large" :src="user.avatar" alt="">
       </a>
 
-      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
+      <p class="desktop-only text-small">{{userThreadsCount}} threads</p>
     </div>
 
     <div class="post-content">
@@ -35,7 +35,6 @@
 </template>
 
 <script>
-    import {countObjectProperties} from '@/utils'
     import PostEditor from './PostEditor'
     export default {
       props: {
@@ -57,7 +56,10 @@
           return this.$store.state.users[this.post.userId]
         },
         userPostsCount () {
-          return countObjectProperties(this.user.posts)
+          return this.$store.getters.userPostsCount(this.post.userId)
+        },
+        userThreadsCount () {
+          return this.$store.getters.userThreadsCount(this.post.userId)
         }
       }
     }
