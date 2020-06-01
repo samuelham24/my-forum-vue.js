6879eb9acb8bd2e07c9a6af6cc24e9960d1c5a30
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-02 01:43:07 +0700

    Update PageThreadCreate.vue

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -3,30 +3,22 @@
 
     <h1>Create new thread in <i>{{forum.name}}</i></h1>
 
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input v-model="title" type="text" id="thread_title" class="form-input" name="title">
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea v-model="text" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-      </div>
-    </form>
+    <ThreadEditor
+    @save="save"
+    @cancel="cancel"
+    />
   </div>
 </template>
 
 <script>
+    import ThreadEditor from '@/components/ThreadEditor'
     export default {
+      components: {
+        ThreadEditor
+      },
       props: {
-        forum: {
-          type: Object,
+        forumId: {
+          type: String,
           required: true
         }
       },
@@ -36,13 +28,23 @@
           text: ''
         }
       },
+      computed: {
+        forum () {
+          return this.$store.state.forums[this.forumId]
+        }
+      },
       methods: {
-        save () {
+        save ({title, text}) {
           this.$store.dispatch('createThread', {
             forumId: this.forum['.key'],
-            title: this.title,
-            text: this.text
+            title,
+            text
+          }).then(thread => {
+            this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
           })
+        },
+        cancel () {
+          this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
         }
       }
     }
