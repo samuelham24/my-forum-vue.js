2d5938caf1f4f920f7d1beb6061505ebf71ab1f9
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-03 04:39:29 +0700

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,12 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  FIREBASE_NODE_ENV: '"production"',
+  FIREBASE_API_KEY: '"AIzaSyAfK82qDhPDM6vAROdC7_yQ8X33B2QEZzI"',
+  FIREBASE_AUTH_DOMAIN: '"my-forum-vue.firebaseapp.com"',
+  FIREBASE_DATABASE_URL: '"https://my-forum-vue.firebaseio.com"',
+  FIREBASE_PROJECT_ID: '"my-forum-vue"',
+  FIREBASE_STORAGE_BUCKET: '"my-forum-vue.appspot.com"',
+  FIREBASE_MESSAGING_SENDER_ID: '"407557273347"',
+  FIREBASE_APP_ID: '"1:407557273347:web:c70219be204e6f0ba0db36"',
+  FIREBASE_MEASUREMENT_ID: '"G-5PNTX6FFHX"'
 }
