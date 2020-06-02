e444923c7bc9d11d36230eb9e2afc33526116860
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-03 04:38:51 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,16 +2,35 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
+import store from '@/store'
 import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate) // register the component globally only when PostListItem loaded
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  beforeCreate () {
+    store.dispatch('fetchUser', {id: store.state.authId})
+  }
 })
