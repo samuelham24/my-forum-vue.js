723dcd53097fd37115839ceb96532007da5eee89
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-03 23:26:16 +0700

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,18 @@ const countObjectProperties = obj => {
   }
   return 0
 }
+
+const removeEmptyProperties = obj => {
+  const objCopy = {...obj}
+  Object.keys(objCopy).forEach(key => {
+    if ([null, undefined].includes(objCopy[key])) {
+      delete objCopy[key]
+    }
+  })
+  return objCopy
+}
+
 export {
-    countObjectProperties
+  countObjectProperties,
+  removeEmptyProperties
 }
