e80862eabb568b9b940f60ae5573ef0ca9601099
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-05 20:03:08 +0700

    Update PageThreadCreate.vue

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
 
     <h1>Create new thread in <i>{{forum.name}}</i></h1>
 
     <ThreadEditor
-    @save="save"
-    @cancel="cancel"
+      ref="editor"
+      @save="save"
+      @cancel="cancel"
     />
   </div>
 </template>
 
 <script>
+    import {mapActions} from 'vuex'
     import ThreadEditor from '@/components/ThreadEditor'
+    import asyncDataStatus from '@/mixins/asyncDataStatus'
     export default {
       components: {
         ThreadEditor
       },
+      mixins: [asyncDataStatus],
       props: {
         forumId: {
           type: String,
@@ -24,27 +28,48 @@
       },
       data () {
         return {
-          title: '',
-          text: ''
+          saved: false
         }
       },
       computed: {
         forum () {
-          return this.$store.state.forums[this.forumId]
+          return this.$store.state.forums.items[this.forumId]
+        },
+        hasUnsavedChanges () {
+          return (this.$refs.editor.form.title || this.$refs.editor.form.text) && !this.saved
         }
       },
       methods: {
+        ...mapActions('threads', ['createThread']),
+        ...mapActions('forums', ['fetchForum']),
         save ({title, text}) {
-          this.$store.dispatch('createThread', {
+          this.createThread({
             forumId: this.forum['.key'],
             title,
             text
           }).then(thread => {
+            this.saved = true
             this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
           })
         },
         cancel () {
           this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
+        }
+      },
+      created () {
+        this.fetchForum({id: this.forumId})
+          .then(() => { this.asyncDataStatus_fetched() })
+      },
+      beforeRouteLeave (to, from, next) {
+        if (this.hasUnsavedChanges) {
+          const confirmed = window.confirm('Are you sure you want to leave? Unsaved changes will be lost.')
+          if (confirmed) {
+            next()
+          } else {
+            next(false)
+          }
+        } else {
+          next()
         }
       }
     }
