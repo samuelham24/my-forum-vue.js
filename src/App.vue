d83f20176fef86cf7e1e2b946c99a75351550e3a
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-05 20:07:45 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-      <div class="container">
-        <router-view/>
-      </div>
+    <TheNavbar/>
+    <div class="container">
+      <router-view :key="$route.path" v-show="showPage" @ready="pageReady"/>
+      <AppSpinner v-show="!showPage"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import TheNavbar from '@/components/TheNavbar'
+  import AppSpinner from '@/components/AppSpinner'
+  import NProgress from 'nprogress'
+  export default {
+    components: {
+      TheNavbar,
+      AppSpinner
+    },
+    data () {
+      return {
+        showPage: false
+      }
+    },
+    methods: {
+      pageReady () {
+        this.showPage = true
+        NProgress.done()
+      }
+    },
+    created () {
+      NProgress.configure({
+        speed: 200,
+        showSpinner: false
+      })
+      NProgress.start()
+      this.$router.beforeEach((to, from, next) => {
+        this.showPage = false
+        NProgress.start()
+        next()
+      })
+    }
+  }
 </script>
 
 <style>
 @import "assets/css/style.css";
+@import "~nprogress/nprogress.css";
+#nprogress .bar {
+  background: #57AD8D;
+}
 </style>
