22d438e7b06849c1e51dc7e0e815e964d601bd97
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-05 20:01:05 +0700

    Update PageCategory.vue

--- src/pages/PageCategory.vue
+++ src/pages/PageCategory.vue
@@ -1,28 +1,40 @@
 <template>
-  <div class="col-full">
+  <div v-if="asyncDataStatus_ready" class="col-full">
     <h1>{{ category.name }}</h1>
-    <CategoryListItem :category="category" />
+    <CategoryListItem :category="category"/>
   </div>
 </template>
 
 <script>
-import CategoryListItem from '@/components/CategoryListItem'
-export default {
-  components: {
-    CategoryListItem
-  },
-  props: {
-    id: {
-      required: true,
-      type: String
+    import {mapActions} from 'vuex'
+    import CategoryListItem from '@/components/CategoryListItem'
+    import asyncDataStatus from '@/mixins/asyncDataStatus'
+    export default {
+      components: {
+        CategoryListItem
+      },
+      mixins: [asyncDataStatus],
+      props: {
+        id: {
+          required: true,
+          type: String
+        }
+      },
+      computed: {
+        category () {
+          return this.$store.state.categories.items[this.id]
+        }
+      },
+      methods: {
+        ...mapActions('categories', ['fetchCategory']),
+        ...mapActions('forums', ['fetchForums'])
+      },
+      created () {
+        this.fetchCategory({id: this.id})
+          .then(category => this.fetchForums({ids: category.forums}))
+          .then(() => { this.asyncDataStatus_fetched() })
+      }
     }
-  },
-  computed: {
-    category () {
-      return this.$store.state.categories[this.id]
-    }
-  }
-}
 </script>
 
 <style scoped>
