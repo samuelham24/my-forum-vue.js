e5bd95df089782188110dc9fb6c59231dee3b449
Samuel Hamonangan <54662264+samuelham24@users.noreply.github.com>
2020-06-05 20:04:13 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import store from '@/store'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
@@ -7,15 +8,17 @@ import ThreadEdit from '@/pages/PageThreadEdit'
 import Category from '@/pages/PageCategory'
 import Forum from '@/pages/PageForum'
 import Profile from '@/pages/PageProfile'
+import Register from '@/pages/PageRegister'
+import SignIn from '@/pages/PageSignIn'
 import NotFound from '@/pages/PageNotFound'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: '/',  // path to the main page
+      path: '/',
       name: 'Home',
-      component: Home  // name of component
+      component: Home
     },
     {
       path: '/category/:id',
@@ -24,46 +27,97 @@ export default new Router({
       props: true
     },
     {
-      path: '/forum/:id',  // path to the main page
+      path: '/forum/:id',
       name: 'Forum',
-      component: Forum,  // name of component
+      component: Forum,
       props: true
     },
     {
       path: '/thread/create/:forumId',
       name: 'ThreadCreate',
       component: ThreadCreate,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
     },
     {
-      path: '/thread/:id',  // path to the spesific thread
+      path: '/thread/:id',
       name: 'ThreadShow',
-      component: ThreadShow,  // name of component
+      component: ThreadShow,
       props: true
     },
     {
       path: '/thread/:id/edit',
       name: 'ThreadEdit',
       component: ThreadEdit,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
     },
     {
       path: '/me',
       name: 'Profile',
       component: Profile,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn,
+      meta: { requiresAuth: true }
     },
     {
       path: '/me/edit',
       name: 'ProfileEdit',
       component: Profile,
-      props: {edit: true}
+      props: {edit: true},
+      meta: { requiresAuth: true }
     },
     {
-      path: '*', // path if page note found, back to home
+      path: '/logout',
+      name: 'SignOut',
+      meta: { requiresAuth: true },
+      beforeEnter (to, from, next) {
+        store.dispatch('signOut')
+          .then(() => next({name: 'Home'}))
+      }
+    },
+    {
+      path: '*',
       name: 'NotFound',
       component: NotFound
     }
   ],
   mode: 'history'
 })
+
+router.beforeEach((to, from, next) => {
+  console.log(`🚦 navigating to ${to.name} from ${from.name}`)
+  store.dispatch('auth/initAuthentication')
+    .then(user => {
+      if (to.matched.some(route => route.meta.requiresAuth)) {
+        // protected route
+        if (user) {
+          next()
+        } else {
+          next({name: 'SignIn', query: {redirectTo: to.path}})
+        }
+      } else if (to.matched.some(route => route.meta.requiresGuest)) {
+        // protected route
+        if (!user) {
+          next()
+        } else {
+          next({name: 'Home'})
+        }
+      } else {
+        next()
+      }
+    })
+})
+
+export default router
